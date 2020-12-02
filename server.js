89c86ed83949a7d4749dd8be6f69e25cff0f3151
Your username <suhaha.git@gmail.com>
2020-12-02 20:58:46 +0200

    server part ready

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const api = require('./server/routes/api')
+const path = require('path')
 
 const app = express()
 
@@ -11,7 +12,7 @@ app.use(express.static(path.join(__dirname, 'node_modules')))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use('/api', api)
+app.use('/', api)
 
 const PORT = 4200
 app.listen(PORT, () => {
