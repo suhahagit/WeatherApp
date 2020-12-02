df6a8ed278c239744f2e57cf7852dfb48128a8a1
Your username <suhaha.git@gmail.com>
2020-12-03 01:29:47 +0200

    logic

---
+++ client/main.js
@@ -0,0 +1,7 @@
+const manager = new dataManager()
+const renderer = new Renderer()
+
+//manager.getDataFromDB()
+// manager.getCityData('Jerusalem')
+//promise.then(manager.saveCity('Jerusalem'))
+//manager.removeCity('Jerusalem')
